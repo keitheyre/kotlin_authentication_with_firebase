cdb1cb5ae660bdecfb76f54542ed2400a6cf1320
keitheyre <keith.eyre91@gmial.com>
2024-01-17 13:42:16 +0000

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Kotlin Authentication with Firebase Application"
+include(":app")
+ 
